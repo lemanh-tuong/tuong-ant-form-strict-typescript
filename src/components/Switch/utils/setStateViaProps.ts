4f8133250481e49fd45f245b02b1e02ef12d854a
lemanh-tuong <letuong05092000@gmail.com>
2023-03-27 16:13:01 +0700

    khởi tạo component Switch

---
+++ src/components/Switch/utils/setStateViaProps.ts
@@ -0,0 +1,11 @@
+import { SwitchProps } from '../@types/Props';
+import { Result } from '../@types/Result';
+
+export const setStateViaProps = (valueProps: SwitchProps['value']): Result => {
+  const isInvalid = valueProps !== null && typeof valueProps !== 'string';
+  if (isInvalid) {
+    console.warn('Invalid input', { value: valueProps });
+    return null;
+  }
+  return valueProps;
+};
