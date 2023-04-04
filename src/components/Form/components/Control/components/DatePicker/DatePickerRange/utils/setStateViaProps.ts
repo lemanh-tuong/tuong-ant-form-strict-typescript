341ccbdcb4f3b5695787876381ba49e514fc5112
lemanh-tuong <letuong05092000@gmail.com>
2023-04-04 10:56:28 +0700

    update

--- src/components/Form/components/Control/components/DatePicker/DatePickerRange/utils/setStateViaProps.ts
+++ src/components/Form/components/Control/components/DatePicker/DatePickerRange/utils/setStateViaProps.ts
@@ -1,11 +1,12 @@
 import dayjs from 'dayjs';
+import { isNil } from 'ramda';
 import { Props } from '../@types/Props';
 import { Result } from '../@types/Result';
 
 export const setStateViaProps = (valueProps: Props['value']): Result => {
   const isInvalid =
-    !!valueProps &&
-    valueProps?.findIndex(item => {
+    !isNil(valueProps) &&
+    valueProps.findIndex(item => {
       return !dayjs.isDayjs(item) || !item.isValid();
     }) !== -1;
   if (isInvalid) {
--- src/components/Form/components/Control/components/Slider/SliderRange/utils/setStateViaProps.ts
+++ src/components/Form/components/Control/components/Slider/SliderRange/utils/setStateViaProps.ts
@@ -1,10 +1,11 @@
+import { isNil } from 'ramda';
 import { Props } from '../@types/Props';
 import { Result } from '../@types/Result';
 
 export const setStateViaProps = (valueProps: Props['value']): Result => {
   const isInvalid =
-    !!valueProps &&
-    valueProps?.findIndex(item => {
+    !isNil(valueProps) &&
+    valueProps.findIndex(item => {
       return typeof item !== 'number';
     }) !== -1;
   if (isInvalid) {
